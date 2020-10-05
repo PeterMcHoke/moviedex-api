9b1b78747be688387faa5fa5f8bab6ce8c63a722
PeterMcHoke <petermchoke@gmail.com>
2020-10-05 14:51:12 -0500

    Working version of Moviedex

--- server.js
+++ server.js
@@ -2,14 +2,44 @@ const express = require('express');
 const morgan = require('morgan');
 const MOVIEDEX = require('./moviedex.json');
 const cors = require('cors');
-const hemlet = require('helmet');
-require('dovenv').config()
+const helmet = require('helmet');
+require('dotenv').config();
 
 const app = express();
 app.use(helmet());
 app.use(cors());
 
 app.use(morgan('dev'));
+app.use(function validateBearerToken(req, res, next) {
+    const authToken = req.get('Authorization');
+    const apiToken = process.env.API_TOKEN;
+    console.log(authToken,apiToken);
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        res.status(401).json({error: 'Unauthorized request'})
+    }
+
+    next();
+})
+
+function handleGetMovies(req, res) {
+    let movies = MOVIEDEX;
+
+    if(req.query.genre) {
+        movies = movies.filter( movie => movie.genre.toLowerCase().includes(req.query.genre.toLowerCase()))
+    }
+
+    if(req.query.country){
+        movies = movies.filter( movie => movie.country.toLowerCase().includes(req.query.country.toLowerCase()))
+    }
+
+    if(req.query.avg_vote){
+        movies = movies.filter( movie => movie.avg_vote >= Number(req.query.avg_vote))
+    }
+
+    res.json(movies)
+
+}
+app.get('/movies', handleGetMovies);
 
 const PORT = 8000;
 app.listen(PORT, () => {
